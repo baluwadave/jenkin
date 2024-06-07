e1960a29673dcf48e70ac54018c80c9a9f99a704
Balu <baluwadave@gmail.com>
2024-06-07 17:46:50 +0530

    --- Auto Git Commit ---

---
+++ terraform.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout the repository containing the Terraform code
+                git branch: 'main', url: 'https://github.com/baluwadave/terraform.git'
+            }
+        }
+
+        stage('Terraform Init') {
+            steps {
+                script {
+                    // Initialize Terraform
+                    sh 'terraform init'
+                }
+            }
+        }
+
+        stage('Terraform Validate') {
+            steps {
+                script {
+                    // Validate Terraform configuration files
+                    sh 'terraform validate'
+                }
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                script {
+                    // Generate and display the execution plan
+                    sh 'terraform plan -out=tfplan -input=false'
+                }
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                script {
+                    // Apply the Terraform plan
+                    sh 'terraform apply -input=false tfplan'
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Clean up the workspace after the build
+            cleanWs()
+        }
+        success {
+            echo 'Terraform applied successfully!'
+        }
+        failure {
+            echo 'Terraform apply failed.'
+        }
+    }
+}
